2e47f6ab1d1a689023bae122384fd5eaaebd021c
Ralph Heinkel <rh@ralph-heinkel.com>
2022-06-17 10:00:19 +0200

    Properly install easyconfig from my personal github repo.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['Click>=7.0', 'imapclient', 'easyconfig']
+requirements = ['Click>=7.0', 'imapclient', 'easyconfig @ git+https://github.com/ralhei/easyconfig.git']
 
 setup_requirements = ['pytest-runner', ]
 
